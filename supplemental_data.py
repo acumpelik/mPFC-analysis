e6e6e60899cb71bd35481fe921f666147551a261
acumpelik <acumpelik@gmail.com>
2024-04-06 12:59:04 +0200

    added JC315 partial data and plotted, some plots aren't generated well yet

--- supplemental_data.py
+++ supplemental_data.py
@@ -6,6 +6,7 @@ rule_change = {
     'JC267': 4,
     'JC274': 12,
     'JC283': 14,
+    'JC315': 14, # needs to be edited
 }
 
 cscheme = {
@@ -16,17 +17,20 @@ cscheme = {
     # 'JC274' : '#49BEAA',
     # 'JC283' : '#1A8FE3',
     'JC258' : '#1b9e77',
-    'JC267' : '#e7298a',
-    'JC274' : '#d95f02',
-    'JC283' : '#7570b3',
+    'JC267' : '#66a61e',
+    'JC274' : '#7570b3',
+    'JC283' : '#d95f02',
+    'JC315' : '#e7298a',
     'JC258_light' : '#e0f3ed',
     'JC267_light' : '#fae8f3',
     'JC274_light' : '#fee8e0',
     'JC283_light' : '#dde3ef',
+    'JC315_light' : '#fef0f7',
     'JC258_medium' : '#d1ede4',
     'JC267_medium' : '#f8dced',
     'JC274_medium' : '#feddd0',
     'JC283_medium' : '#d1d9ea',
+    'JC315_medium' : '#fdeaf3',
     'C' : "#593510",
     'H' : "#fcbe15",
     'S' : "#d2c28f",
@@ -47,4 +51,5 @@ reward_arms = {
     'JC267': [3, 7],
     'JC274': [8, 4, 5],
     'JC283': [8, 3, 5],
+    'JC315': [3, 4, 7],
 }
